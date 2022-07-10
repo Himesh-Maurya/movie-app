8810080a608a0e9813483d9701fe5ac3b76a3d27
Himesh-Maurya <himeshmauurya@gmail.com>
2022-07-10 21:54:28 +0530

    adding favourite removing favourites

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,11 +5,34 @@
 
 //action types
 export const ADD_MOVIES = "ADD_MOVIES";
-
+// export const ADD_FAVOURITE = "ADD_FAVOURITE";
+export const ADD_TO_FAVOURITES = "ADD_TO_FAVOURITES";
+export const REMOVE_FROM_FAVOURITES = "REMOVE_FROM_FAVOURITES";
+export const SET_SHOW_FAVOURITES = "SET_SHOW_FAVOURITES";
 //action creators function
 export function addMovies(movies) {
   return {
     type: ADD_MOVIES,
     movies: movies,
+  };
+}
+
+export function addFavourite(movie) {
+  return {
+    type: ADD_TO_FAVOURITES,
+    movie: movie,
+  };
+}
+export function removeFromFavourites(movie) {
+  return {
+    type: REMOVE_FROM_FAVOURITES,
+    movie: movie,
+  };
+}
+
+export function setShowFavourites(val) {
+  return {
+    type: SET_SHOW_FAVOURITES,
+    val: val,
   };
 }
--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import { data } from "../data";
 import React from "react";
 import Navbar from "./Navbar";
 import MovieCard from "./MovieCard";
-import { addMovies } from "../actions";
+import { addMovies, setShowFavourites } from "../actions";
 class App extends React.Component {
   componentDidMount() {
     const { store } = this.props;
@@ -15,28 +15,60 @@ class App extends React.Component {
     store.dispatch(addMovies(data));
     console.log("STATE", store.getState());
   }
+  isMovieFavourite = (movie) => {
+    const { favourites } = this.props.store.getState(); //yaha favourite wala array aa jayega favourites me
+    const index = favourites.indexOf(movie);
+    if (index !== -1) {
+      //found the movie
+      return true;
+    }
+    return false;
+  };
+  onChangeTab = (val) => {
+    this.props.store.dispatch(setShowFavourites(val));
+  };
   render() {
-    const movies = this.props.store.getState(); //{list:[m1,m2..],favourites:[]}
+    // const movies = this.props.store.getState(); //{list:[m1,m2..],favourites:[]}
     //to ye mujhe object return krega
+    const { list, favourites, showFavourites } = this.props.store.getState();
     console.log("render....", this.props.store.getState());
+    const displayMovies = showFavourites ? favourites : list;
     return (
       <div className="App">
         <Navbar />
         <div className="main">
           <div className="tabs">
-            <div className="tab">Movies</div>
-            <div className="tab">Favourite</div>
+            <div
+              className={`tab ${showFavourites ? "" : "active-tabs"} `}
+              onClick={() => this.onChangeTab(false)}
+            >
+              Movies
+            </div>
+            <div
+              className={`tab ${showFavourites ? "active-tabs" : ""} `}
+              onClick={() => this.onChangeTab(true)}
+            >
+              Favourite
+            </div>
           </div>
           <div className="list">
-            {movies.list.map(
+            {displayMovies.map(
               (
                 movie,
                 index //uss movie ka index
               ) => (
-                <MovieCard movie={movie} key={`movies-${index}`} />
+                <MovieCard
+                  movie={movie}
+                  key={`movies-${index}`}
+                  dispatch={this.props.store.dispatch}
+                  isFavourite={this.isMovieFavourite(movie)}
+                />
               )
             )}
           </div>
+          {displayMovies.length === 0 ? (
+            <div className="no-movies">No movies to display!</div>
+          ) : null}
         </div>
       </div>
     );
